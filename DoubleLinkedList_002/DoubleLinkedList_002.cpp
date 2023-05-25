91a9de2283b45b80d77f15315b231b95f1a4a2e8
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-05-25 11:06:44 +0700

    membuat method addNode pada class DoubleLinkedList

--- DoubleLinkedList_002/DoubleLinkedList_002.cpp
+++ DoubleLinkedList_002/DoubleLinkedList_002.cpp
@@ -29,3 +29,44 @@ public:
 DoubleLinkedList::DoubleLinkedList() {
 	START = NULL;
 }
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\n Enter the name of the student: ";
+	cin.ignore();
+	getline(cin, nm);
+	Node* newNode = new Node();
+	newNode->noMhs = nim;
+	newNode->name = nm;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START;
+		if (START != NULL)
+			START->prev = newNode;
+		newNode->prev = NULL;
+		START = newNode;
+		return;
+	}
+	
+	Node* current = START;
+	while (current->next != NULL && nim > current->next->noMhs)
+		current = current->next;
+
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nDuplicate roll numbers not allowed" << endl;
+		return;
+	}
+
+	newNode->next = current->next;
+	newNode->prev = current;
+	if (current->next != NULL)
+		current->next->prev = newNode;
+	current->next = newNode;
+}
